0ad4fa4ee5862acd8ef5af2cc8c556daae783ccf
LylyG <lgalarza@spoonflower.com>
2016-03-11 16:01:44 -0500

    Generate Answer scaffold. Added question_id to new table.

--- db/migrate/20160311210018_create_answers.rb
+++ db/migrate/20160311210018_create_answers.rb
@@ -3,8 +3,8 @@ class CreateAnswers < ActiveRecord::Migration
     create_table :answers do |t|
       t.integer :question_number
       t.string :question_response
-      t.integer :survey_id
       t.integer :taker_id
+      t.integer :question_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311181612) do
+ActiveRecord::Schema.define(version: 20160311210018) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_number"
     t.string   "question_response"
-    t.integer  "survey_id"
     t.integer  "taker_id"
+    t.integer  "question_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-  end
-
-  create_table "long_questions", force: :cascade do |t|
-    t.integer  "survey_id"
-    t.integer  "question_number"
-    t.text     "question"
-    t.text     "response"
-    t.text     "description"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
 
   create_table "questions", force: :cascade do |t|
@@ -37,17 +27,7 @@ ActiveRecord::Schema.define(version: 20160311181612) do
     t.integer  "question_number"
     t.text     "question_text"
     t.text     "description"
-    t.string   "question_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "short_questions", force: :cascade do |t|
-    t.integer  "survey_id"
-    t.integer  "question_number"
-    t.text     "question"
-    t.text     "response"
-    t.text     "description"
+    t.string   "type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
@@ -71,16 +51,6 @@ ActiveRecord::Schema.define(version: 20160311181612) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "yes_no_questions", force: :cascade do |t|
-    t.integer  "survey_id"
-    t.integer  "question_number"
-    t.text     "question"
-    t.boolean  "response"
-    t.text     "description"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
