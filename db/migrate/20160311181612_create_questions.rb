33182519388a03a8ff524b88908424d279841ec6
LylyG <lgalarza@spoonflower.com>
2016-03-11 13:16:43 -0500

    Generate Question scaffold

---
+++ db/migrate/20160311181612_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.integer :survey_id
+      t.integer :question_number
+      t.text :question_text
+      t.text :description
+      t.string :type
+
+      t.timestamps null: false
+    end
+  end
+end
