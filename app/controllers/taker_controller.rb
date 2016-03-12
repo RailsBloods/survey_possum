44ed741691c393debf7ab803b8d37eb008fc5d02
LylyG <lgalarza@spoonflower.com>
2016-03-12 16:45:25 -0500

    Add notes to explain reasoning in the takers controller.

--- app/controllers/taker_controller.rb
+++ app/controllers/taker_controller.rb
@@ -8,6 +8,16 @@ class TakerController < ApplicationController
     end
   end
 
+  #The idea with separating takers from the survey controller is to give them distinct views.
+  # The user would likely want to redirect to a page that displays all saved surveys (published and unpublished).
+  # The taker should see a page with the published survey and not be able to type it into the URL. 
+
+  def required_questions
+    @survey = Survey.find(params[:id])
+    if @survey.question.required == 1
+    end
+  end
+
   def survey_params
     params.require(:survey).permit(:title, :description, :user_id, :publish,
         questions_attributes: [:id, :question_type, :question_number, :question_text, :response, :description, :required, :_destroy,
