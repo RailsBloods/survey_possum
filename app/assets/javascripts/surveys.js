e01faf12c41dda000790b3d9bfbec838f7432a93
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-12 17:30:57 -0500

    adding changes

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -1,15 +1,14 @@
-$(function(){
-'use strict';
-
-var clickCounter = 1;
-
-$(".question-button").on("click", function(){
-   console.log('hello');
-   $(".unordered-list").append('<li class="question-selector"><input placeholder="Question Number" type="number" name="survey[questions_attributes]['+clickCounter+'][question_number]" id="survey_questions_attributes_'+clickCounter+'_question_number"><select name="survey[questions_attributes]['+clickCounter+'][question_type]" id="survey_questions_attributes_'+clickCounter+'_question_type"><option value="Yes/No">Yes/No</option><option value="Short Answer">Short Answer</option><option value="Long Answer">Long Answer</option></select><input placeholder="Question" type="text" name="survey[questions_attributes]['+clickCounter+'][question_text]" id="survey_questions_attributes_'+clickCounter+'_question_text"><textarea placeholder="Description (Optional)" name="survey[questions_attributes]['+clickCounter+'][description]" id="survey_questions_attributes_'+clickCounter+'_description"></textarea></li>');
-   clickCounter++;
-   console.log(clickCounter);
-});
-
-
-// $(".question-selector").append($('<li class="question-selector"<input placeholder="Question Number" type="number" name="survey[questions_attributes][0][question_number]" id="survey_questions_attributes_0_question_number"></li>'));
-});//end of line
+// $(function(){
+// 'use strict';
+//
+// var clickCounter = 1;
+//
+// $(".question-button").on("click", function(){
+//    console.log('hello');
+//    $(".unordered-list").append('<li class="question-selector"><input placeholder="Question Number" type="number" name="survey[questions_attributes]['+clickCounter+'][question_number]" id="survey_questions_attributes_'+clickCounter+'_question_number"><select name="survey[questions_attributes]['+clickCounter+'][question_type]" id="survey_questions_attributes_'+clickCounter+'_question_type"><option value="Yes/No">Yes/No</option><option value="Short Answer">Short Answer</option><option value="Long Answer">Long Answer</option></select><input placeholder="Question" type="text" name="survey[questions_attributes]['+clickCounter+'][question_text]" id="survey_questions_attributes_'+clickCounter+'_question_text"><textarea placeholder="Description (Optional)" name="survey[questions_attributes]['+clickCounter+'][description]" id="survey_questions_attributes_'+clickCounter+'_description"></textarea></li>');
+//    clickCounter++;
+//    console.log(clickCounter);
+// });
+//
+//
+// });//end of line
