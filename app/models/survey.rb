d66317e3ad1fa7c1ae97e528e8fac7492856acd5
LylyG <lgalarza@spoonflower.com>
2016-03-12 13:39:28 -0500

    Added validations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,7 @@
 class Survey < ActiveRecord::Base
   has_many :questions
   has_many :answers, through: :questions
+  validates :title, presence: true 
 
   accepts_nested_attributes_for :questions,
       reject_if: :all_blank,
