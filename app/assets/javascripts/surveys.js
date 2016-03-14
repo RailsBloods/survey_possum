2da2f30d359c87eab8d151f8eb30cf079b094697
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-14 12:57:58 -0400

    adding styling for the form survey page

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -18,15 +18,18 @@ $(function(){
   var clickCounter = 1;
   $(".question-button").on("click", function(){
      console.log('hello');
-     $(".surveys-ordered-list").append('<li class="question-selector"><input placeholder="Question #" type="number" name="survey[questions_attributes]['+clickCounter+'][question_number]" id="survey_questions_attributes_'+clickCounter+'_question_number"><select name="survey[questions_attributes]['+clickCounter+'][question_type]" id="survey_questions_attributes_'+clickCounter+'_question_type"><option value="">Select Question Type</option><option value="Yes/No">Yes/No</option><option value="Short Answer">Short Answer</option><option value="Long Answer">Long Answer</option></select><input placeholder="What question do you want to ask?" type="text" name="survey[questions_attributes]['+clickCounter+'][question_text]" id="survey_questions_attributes_'+clickCounter+'_question_text"><textarea placeholder="Optional Description" name="survey[questions_attributes]['+clickCounter+'][description]" id="survey_questions_attributes_'+clickCounter+'_description"></textarea></li>');
+     $(".surveys-ordered-list").append('<li class="question-selector"><input placeholder="#" type="number" name="survey[questions_attributes]['+clickCounter+'][question_number]" id="survey_questions_attributes_'+clickCounter+'_question_number"><select name="survey[questions_attributes]['+clickCounter+'][question_type]" id="survey_questions_attributes_'+clickCounter+'_question_type"><option value="">Select Question Type</option><option value="Yes/No">Yes/No</option><option value="Short Answer">Short Answer</option><option value="Long Answer">Long Answer</option></select><input placeholder="What question do you want to ask?" type="text" name="survey[questions_attributes]['+clickCounter+'][question_text]" id="survey_questions_attributes_'+clickCounter+'_question_text"><textarea placeholder="Optional Description" name="survey[questions_attributes]['+clickCounter+'][description]" id="survey_questions_attributes_'+clickCounter+'_description"></textarea></li>');
      clickCounter++;
      console.log(clickCounter);
   });
 
  //shuffles the question you click before the one before
-  $(".question-selector").click(function() {
-     $(this).insertBefore($(this).prev());
+  $(".fa-chevron-up").click(function() {
+    $(this).parents(".question-selector").insertBefore($(this).parents(".question-selector").prev());
   });
 
+  $(".fa-chevron-down").click(function() {
+    $(this).parents(".question-selector").insertAfter($(this).parents(".question-selector").next());
+  });
 
 });
