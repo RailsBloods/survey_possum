c8448db896c743c39ecef46a8d88fe902a396678
Greg Turmel <gregturmel@gmail.com>
2016-03-14 12:58:00 -0400

    add delete button

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -4,20 +4,21 @@ $(function(){
 var clickCounter = 1;
 
 $(".question-button").on("click", function(){
-   $(".ordered-list").append('<li class="question-selector"><input placeholder="Question Number" type="number" name="survey[questions_attributes]['+clickCounter+'][question_number]" id="survey_questions_attributes_'+clickCounter+'_question_number"><span class = "remove-button">x</span><select name="survey[questions_attributes]['+clickCounter+'][question_type]" id="survey_questions_attributes_'+clickCounter+'_question_type"><option value="Yes/No">Yes/No</option><option value="Short Answer">Short Answer</option><option value="Long Answer">Long Answer</option></select><input placeholder="Question" type="text" name="survey[questions_attributes]['+clickCounter+'][question_text]" id="survey_questions_attributes_'+clickCounter+'_question_text"><textarea placeholder="Description (Optional)" name="survey[questions_attributes]['+clickCounter+'][description]" id="survey_questions_attributes_'+clickCounter+'_description"></textarea></li>');
+   $(".surveys-ordered-list").append('<li class="question-selector"><input placeholder="Question Number" type="number" name="survey[questions_attributes]['+clickCounter+'][question_number]" id="survey_questions_attributes_'+clickCounter+'_question_number"><select name="survey[questions_attributes]['+clickCounter+'][question_type]" id="survey_questions_attributes_'+clickCounter+'_question_type"><option value="Yes/No">Yes/No</option><option value="Short Answer">Short Answer</option><option value="Long Answer">Long Answer</option></select>              <span class="remove-button">x</span><input placeholder="Question" type="text" name="survey[questions_attributes]['+clickCounter+'][question_text]" id="survey_questions_attributes_'+clickCounter+'_question_text"><textarea placeholder="Description (Optional)" name="survey[questions_attributes]['+clickCounter+'][description]" id="survey_questions_attributes_'+clickCounter+'_description"></textarea></li>');
    clickCounter++;
    console.log(clickCounter);
 }); //end of add question item JS
 
 $(".remove-button").on("click", function(){
    $(this).parent().remove();
-})//end of remove question item JS
+   console.log('this works');
+});//end of remove question item JS
 
 
   $('#survey-menu-bar').click(function(){
     $('.left-survey-nav').toggleClass('show');
     console.log("this works");
-  })
+});
 
   $('.survey-box').hover(function(){
     $(this).toggleClass('hover');
